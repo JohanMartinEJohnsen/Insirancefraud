16e8b93330eed51850f6d39d4fabd5992d5ea4e9
Johan Martin Johnsen <johan.martin.johnsen@fremtind.no>
2021-07-12 15:00:43 +0200

    final

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import firstImg from './Assets/pictures/firstImg.png'
 import secondImg from './Assets/pictures/secondImg.png'
 import thirdImg from './Assets/pictures/thirdImg.png'
 //import global from './components/globalNum';
+import LastPage from './components/LastPage';
+//import { Button } from 'react-native-elements';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -16,8 +18,8 @@ function App() {
         {count === 0 && <div><img className="screenImg" src={firstImg} onClick={() => setCount(count + 1)}></img></div>}
         {count === 1 && <div><img className="screenImg" src={secondImg} onClick={() => setCount(count + 1)}></img></div>}
         {count === 2 && <div><img className="screenImg" src={thirdImg} onClick={() => setCount(count + 1)}></img></div>}
-        {count === 3 && <CaseDisplay></CaseDisplay>}
-        {global.lastPage === true && <div><h3>Hei</h3></div>}
+        {count === 3 && <div><CaseDisplay></CaseDisplay><button onClick={() => setCount(count + 1)}>Next task</button></div>}
+        {count === 4 &&<div><LastPage/></div>}
       </div>
   );
 }
--- src/components/LastPage.tsx
+++ src/components/LastPage.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import Swal from "sweetalert2";
 
-import lineup from 'Assets/pictures/lineUp.png'
-import { Button } from 'react-native-elements';
+import lineup from '../Assets/pictures/lineUp.png'
 
 const LastPage = () => {
     
@@ -10,10 +9,12 @@ const LastPage = () => {
         <div>
             <img src={lineup}></img>
             <div>
-                <Button title="Nummer 1" onPress={()=> Swal.fire('Du svarte feil!', '', 'error')}></Button>
-                <Button title="Nummer 2" onPress={() => Swal.fire('Du svarte riktig!', '', 'success')}></Button>
-                <Button title="Nummer 3" onPress={()=> Swal.fire('Du svarte feil!', '', 'error')}></Button>
-                <Button title="Nummer 4" onPress={()=> Swal.fire('Du svarte feil!', '', 'error')}></Button>
+                <button title="Nummer 1" onClick={()=> Swal.fire('Du svarte feil!', '', 'error')}>Nummer 1</button>
+                <button title="Nummer 2" onClick={() => Swal.fire('Du svarte riktig!', '', 'success')}>Nummer 2</button>
+                <button title="Nummer 3" onClick={()=> Swal.fire('Du svarte feil!', '', 'error')}>Nummer 3</button>
+                <button title="Nummer 4" onClick={() => Swal.fire('Du svarte riktig!', '', 'success')}>Nummer 4</button>
+                <button title="Nummer 5" onClick={()=> Swal.fire('Du svarte feil!', '', 'error')}>Nummer 5</button>
+
             </div>
 
         </div>
